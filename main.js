f84f287e1026fb5e6e8d90454c5f5ad96bcc87ba
Dominik Hauerstein <dcdominoes@gmail.com>
2022-01-12 01:32:06 +0000

    added error handling, and actual downloading of JSON data

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
+const errMsg = document.getElementById("errorMsg");
+document.getElementById("btnSearch").addEventListener('click', function(){
+    errMsg.style.display = "none";
+    doSearch(document.getElementById("txtSearch").value);
+})
+
 doSearch = input => {
-    verifyText(input);
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto' + input)
-        .then(response => response.json())
-        .then(json => data = json);
+    if (!verifyText(input)) return;
+    console.log("text verified");
+    fetch('https://pokeapi.co/api/v2/pokemon/' + input).then(response => {
+        if (!response.ok)
+            throw new Error("response returned: " + response.status);
+        else
+            response.json().then(json => document.getElementById("content").appendChild(document.createTextNode(JSON.stringify(json, null, 4))));
+    }).catch(err => {
+        console.error("Error: ", err)
+        displayErrorMessage();
+    });
 }
 
-verifyText = text => {
-    
+displayErrorMessage = () => {
+    errMsg.style.display = "block";
+}
+
+verifyText = txt => {
+    if (!txt) return false; //if empty 
+    return (/^[a-zA-Z]*$/.test(txt) || /^\d+$/.test(txt))
 }
