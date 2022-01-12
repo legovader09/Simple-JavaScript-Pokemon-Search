d28e7b6c66c1204d98484c426fac465198fe7f70
Dominik Hauerstein <dcdominoes@gmail.com>
2022-01-12 22:03:16 +0000

    added finishing touches

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const url = new URL(window.location.href);
 const errMsg = document.getElementById("errorMsg");
+const pokeInfo = document.getElementById("pokeInfo");
 const searchTxt = document.getElementById("txtSearch");
 
 /// FUNCTIONS
@@ -17,18 +18,36 @@ doSearch = input => {
     if (!verifyText(input)) return displayErrorMessage();
     console.log("text verified");
     fetch('https://pokeapi.co/api/v2/pokemon/' + input).then(response => {
-        if (!response.ok)
+        if (!response.ok) 
             throw new Error("response returned: " + response.status);
         else
-            response.json().then(json => document.getElementById("content").appendChild(document.createTextNode(JSON.stringify(json, null, 4))));
+            response.json().then(json => showPokemonInfo(json));
     }).catch(err => {
-        console.error(err)
+        console.error(err);
         displayErrorMessage();
     });
 }
 
+showPokemonInfo = obj => {
+    // TODO: change information on page.
+    console.log(obj);
+    let output= "";
+    for (let i = 0; i < obj.abilities.length; i++) {
+        output += obj.abilities[i].ability.name + ", ";
+        output.charAt(0).toUpperCase();
+    }
+    output = output.replace(/,(?=[^,]*$)/, "");
+    document.getElementById("objInfo").innerHTML = 
+    `
+        Name: ${obj.name} <br>
+        Abilities: ${output} <br>
+    `;
+    document.getElementById("objIMG").src = obj.sprites.other["official-artwork"].front_default;
+}
+
 displayErrorMessage = () => {
     errMsg.style.display = "block";
+    pokeInfo.style.display = "none";
 }
 
 verifyText = txt => {
