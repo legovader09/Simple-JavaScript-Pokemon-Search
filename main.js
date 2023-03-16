7af3e7c722cbe0ad847ba414098c9f0397c5d3a8
Dominik H <dcdominoes@gmail.com>
2023-03-16 00:28:30 +0000

    Added random search

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const url = new URL(window.location.href);
-const errMsg = document.getElementById("errorMsg");
-const pokeInfo = document.getElementById("pokeInfo");
-const searchTxt = document.getElementById("txtSearch");
+const errMsg = document.getElementById('errorMsg');
+const pokeInfo = document.getElementById('pokeInfo');
+const searchTxt = document.getElementById('txtSearch');
 
 /// FUNCTIONS
 const checkParams = () => {
-  const params = url.searchParams.get("q");
-  if (params) {
-    searchTxt.value = params;
-    doSearch(params);
+  const query = url.searchParams.get('q');
+  if (query) {
+    searchTxt.value = query;
+    return doSearch(query);
   }
 }
 
 const doSearch = input => {
-  url.searchParams.set("q", input);
+  if (input === 'random') input = `${Math.round(Math.random() * 1000)}`;
+  url.searchParams.set('q', input);
   window.history.replaceState(null, null, url);
   input = input.toLowerCase().trim();
   if (!verifyText(input)) return displayErrorMessage();
@@ -21,7 +22,7 @@ const doSearch = input => {
     if (response.ok) 
       response.json().then(json => showPokemonInfo(json));
     else
-      throw new Error("response returned: " + response.status);
+      throw new Error('response returned: ' + response.status);
   }).catch(err => {
     console.error(err);
     displayErrorMessage(true);
@@ -34,34 +35,34 @@ const doSearch = input => {
  */
 const showPokemonInfo = obj => {
   searchTxt.value = obj.name;
-  let output = "";
+  let output = '';
   for (let i = 0; i < obj.abilities.length; i++) {
     output += `${obj.abilities[i].ability.name}, `;
   }
-  output = output.replace(/,(?=[^,]*$)/, "");
-  let stats = "<ul>";
+  output = output.replace(/,(?=[^,]*$)/, '');
+  let stats = '<ul>';
   for (let i = 0; i < obj.stats.length; i++) {
     stats += `<li>${obj.stats[i].stat.name}: ${obj.stats[i].base_stat}</li>`
   }
-  stats += "</ul>";
-  let types = "";
+  stats += '</ul>';
+  let types = '';
   for (let i = 0; i < obj.types.length; i++) {
     types += `${obj.types[i].type.name}, `;
   }
-  types = types.replace(/,(?=[^,]*$)/, "");
-  document.getElementById("objInfo").innerHTML = 
+  types = types.replace(/,(?=[^,]*$)/, '');
+  document.getElementById('objInfo').innerHTML = 
   `
-    <span class="label">ID</span>: ${obj.id} <br>
-    <span class="label">Name</span>: ${obj.name} <br>
-    <span class="label">Abilities</span>: ${output} <br>
-    <span class="label">Types</span>: ${types}<br>
-    <span class="label">Stats</span>: <br>
+    <span class='label'>ID</span>: ${obj.id} <br>
+    <span class='label'>Name</span>: ${obj.name} <br>
+    <span class='label'>Abilities</span>: ${output} <br>
+    <span class='label'>Types</span>: ${types}<br>
+    <span class='label'>Stats</span>: <br>
     ${stats}
-    <a id="prevPoke">Previous</a> | <a id="nextPoke">Next</a>
+    <a id='prevPoke'>Previous</a> | <a id='nextPoke'>Next</a>
   `;
-  document.getElementById("nextPoke").addEventListener("click", () => {btnNextPokemon(obj.id + 1)});
-  document.getElementById("prevPoke").addEventListener("click", () => {btnNextPokemon(obj.id - 1)});
-  document.getElementById("objIMG").src = obj.sprites.other["official-artwork"].front_default ?? 'sadmon.svg';
+  document.getElementById('nextPoke').addEventListener('click', () => {btnNextPokemon(obj.id + 1)});
+  document.getElementById('prevPoke').addEventListener('click', () => {btnNextPokemon(obj.id - 1)});
+  document.getElementById('objIMG').src = obj.sprites.other['official-artwork'].front_default ?? 'sadmon.svg';
   displayErrorMessage(false);
 }
 
@@ -71,9 +72,9 @@ const btnNextPokemon = (id) => {
 }
 
 const displayErrorMessage = (enable) => {
-  document.getElementById("content").style.display = "block";
-  errMsg.style.display = enable ? "block" : "none";
-  pokeInfo.style.display = enable ? "none" : "flex";
+  document.getElementById('content').style.display = 'block';
+  errMsg.style.display = enable ? 'block' : 'none';
+  pokeInfo.style.display = enable ? 'none' : 'flex';
 }
 
 const verifyText = txt => {
@@ -82,8 +83,13 @@ const verifyText = txt => {
 }
 
 /// ON LOAD
-document.getElementById("btnSearch").addEventListener('click', function(){
+document.getElementById('btnSearch').addEventListener('click', function(){
   if (searchTxt.value.trim().length > 0) doSearch(searchTxt.value);
+})
+
+document.getElementById('btnRandom').addEventListener('click', function(){
+  searchTxt.value = Math.round(Math.random() * 1000);
+  doSearch(searchTxt.value);
 })
 
 searchTxt.addEventListener('keyup', (e) => e.key === 'Enter' && doSearch(searchTxt.value));
